8d213efd5035ac00dde6b5f72b82ebe5b1a60e33
Hwillows <hannah.willows@hotmail.co.uk>
2022-09-04 13:05:07 +0100

    edited home page

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,22 +1,48 @@
 import React from "react";
 import "../App.css";
+import Hannah from "./HannahPic.jpg";
 
 function Homepage() {
   return (
     <div>
       <div className="homepage-background"></div>
-      <div className="container text-center">
-        <div className="row row-cols-1">
-          <div className="col">
-            <h2>About</h2>
+      <div className="container">
+        <div className="row row-cols-2">
+          <div className="col-7">
+            <div className="container ">
+              <div className="row row-cols-1">
+                <div className="col">
+                  <h2 className="mt-3 mb-3">
+                    Increasing your love for your existing wardrobe
+                  </h2>
+                </div>
+                <div className="col">
+                  <p>
+                    Slow Down fashion aims to reduce fast fashion. Here you can
+                    visualise your clothes in a new combination, giving you a
+                    whole new outfit from your existing wardrobe. You can help
+                    to keep your wardrobe organised by marking items you want to
+                    re-home. Slow Down Fashion also includes a platform to share
+                    and find more sustainable fashion brands, to help make your
+                    next purchase a more planet and people friendly one.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col">
-            <p>
-              Slow Down Fashion aims to reduce fast fashion. You can visualise a
-              brand new outfit from your existing wardrobe. You can mark which
-              clothes you want to re-home. You can share and find new
-              sustainable clothing brands.
-            </p>
+          <div className="col-5 ">
+            <div className="d-flex justify-content-end text-center">
+              <div>
+                <img
+                  src={Hannah}
+                  alt="Hannah Willows"
+                  className="img-thumbnail w-25 rounded-5 mx-4 mt-4 "
+                />
+                <p className="mx-5">
+                  Created by <br></br>Hannah Willows
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
